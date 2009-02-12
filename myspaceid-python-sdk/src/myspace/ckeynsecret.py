fc8c6f76e6637191bfd92e3211dbbb946efed5e1
cnanga <cnanga@9d9125b6-f877-11dd-8c4e-4bbb75a408b7>
2009-02-12 18:59:42 +0000

    Removing my key/secret

--- myspaceid-python-sdk/src/myspace/ckeynsecret.py
+++ myspaceid-python-sdk/src/myspace/ckeynsecret.py
@@ -1,5 +1,5 @@
 # Enter your app specific consumer key and secret from MySpace here
 #
 
-CONSUMER_KEY    = 'c413ba05eac54b20a40b6a6bad6f08ef'
-CONSUMER_SECRET = '8c4499ce452e449ea34ab052b27463f1'
+CONSUMER_KEY    = '<YOUR_CONSUMER_KEY_HERE>'
+CONSUMER_SECRET = '<YOUR_CONSUMER_SECRET_HERE>'
